49df32cf26a81e634f3f1f463318b1ee966bcbf6
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-25 14:29:04 -0300

    feat: update config dotnet environment with docker

--- dotnet/dotnet-environment-windows/setup.ps1
+++ dotnet/dotnet-environment-windows/setup.ps1
@@ -3,15 +3,41 @@
 
 # development environment on Windows 
 $dotnetPath = "C:\dev\dotnet-environment\dotnet"
+$sqlPath = "C:\dev\dotnet-environment\mssql"
+$redisPath = "C:\dev\dotnet-environment\redisdb"
 
+# check dotnet path exists
+Write-Host "Checking .NET Core directory!"
 If (Test-Path -Path $dotnetPath) {
-    Write-Host "Directory " $dotnetPath " already exists!" -ForegroundColor "Green"
+    Write-Host "Directory " $dotnetPath " already exists!"
 } else {
-    Write-Host "Creating directory!"
-    mkdir -p C:\dev\dotnet-environment
+    Write-Host "Creating .NET Core directory!" -ForegroundColor "Green"
+    mkdir -p $dotnetPath
 }
 
+# check SQL Server path exists
+Write-Host "Checking SQL Server directory!"
+If (Test-Path -Path $sqlPath) {
+    Write-Host "Directory " $sqlPath " already exists!" 
+} else {
+    Write-Host "Creating SQL Server directory!" -ForegroundColor "Green"
+    mkdir -p $sqlPath
+}
+
+# check Redis path exists
+Write-Host "Checking Redis directory!"
+If (Test-Path -Path $redisPath) {
+    Write-Host "Directory " $redisPath " already exists!" 
+} else {
+    Write-Host "Creating SQL Server directory!" -ForegroundColor "Green"
+    mkdir -p $redisPath
+}
+
+# creating network dev
+# docker network create --driver bridge dev-network
+
 # criar os containers do SQL Server e do Redis
+docker-compose up -d
 
 # roda o comando para criar o container do dotnet
-docker run --rm --volume $dotnetPath":/srv/app" --workdir "/srv/app" --publish 5000:5000 -it mcr.microsoft.com/dotnet/sdk:3.1 bash
+docker run  --name dotnet --rm --volume $dotnetPath":/srv/app" --workdir "/srv/app" --publish 5000:5000 -it --network dev-network mcr.microsoft.com/dotnet/sdk:3.1 bash
